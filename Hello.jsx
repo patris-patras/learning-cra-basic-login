2dd787f9ca34fc7ded3c76810d26b58476917051
Patricia Patras <patras.patricia@gmail.com>
2022-05-09 17:48:13 +0300

    Validate done. Routing to solve

---
+++ Hello.jsx
@@ -0,0 +1,11 @@
+import { name } from './basic-cra-login/src/App';
+
+export const Hello = () => {
+  return (
+    <div>
+      <p>Hello {name}</p>
+    </div>
+  );
+};
+
+export default Hello;
--- basic-cra-login/src/App.jsx
+++ basic-cra-login/src/App.jsx
@@ -1,30 +1,70 @@
 import './App.css';
+import { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function App() {
+  const history = useHistory();
+
+  const [formData, updateFormData] = useState({
+    name: '',
+    password: '',
+  });
+
+  const { name, password } = formData;
+
+  function validateForm(event) {
+    event.preventDefault();
+
+    if (name == 'Patricia' && password == 'parola') {
+      alert('before routing');
+      // routing in pag de hello page
+      history.push('/');
+      alert('after routing');
+    } else {
+      alert('Wrong username or password');
+    }
+  }
+
+  function onChange(event) {
+    updateFormData({
+      ...formData,
+
+      [event.target.name]: event.target.value,
+    });
+  }
+
   return (
     <div className='App'>
       <div className='login-panel'>
         <header className='App-header'>
           <p>Login</p>
-
-          <form action=''>
+          <form onSubmit={validateForm}>
             <div className='input'>
               <label for='name'>Name:</label>
-              <input name='name' placeholder='Name' type='text' required />
-            </div>
-            <div className='input'>
-              <label for='email'>E-mail address:</label>
-              <input type='email' id='email' placeholder='Email' required />
+              <input
+                name='name'
+                type='text'
+                value={name}
+                onChange={onChange}
+                placeholder='Name'
+                required
+              />
             </div>
             <div className='input'>
               <label for='password'>Password:</label>
               <input
                 type='password'
-                id='password'
+                name='password'
+                value={password}
+                onChange={onChange}
                 placeholder='Password'
                 required
               />
             </div>
+            <button className='button' type='submit' title='Login'>
+              Login
+            </button>
           </form>
         </header>
       </div>
